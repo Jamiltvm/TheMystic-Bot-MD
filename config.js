c009ce114b9ef9618de56e9d8a158fe468412cd5
Jamiltvm <163384618+Jamiltvm@users.noreply.github.com>
2024-03-14 07:47:26 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "50583854994"
 global.confirmCode = ""
 
 global.owner = [
